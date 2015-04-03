a36021e4afb8c98e0bcb9d8bab196afad0c93d59
Scott Stadt <scott.stadt@gmail.com>
2015-04-02 23:11:22 -0500

    Moved functions out of global namespace and split tracking up between bag and bank

--- Stockpile.lua
+++ Stockpile.lua
@@ -5,7 +5,7 @@
 
 Stockpile = CreateFrame("Frame", "Stockpile", UIParent)
 
-local tooltipLinesAdded = false
+Stockpile.tooltipLinesAdded = false
 
 -- ------------------------
 -- EVENT ACTIONS
@@ -19,14 +19,18 @@ Stockpile.actions = {
       Stockpile.character = UnitName('player')
       Stockpile.realm = GetRealmName()
 
-      Stockpile_InitializeInventoryData()
-      Stockpile_ScanBags(0, NUM_BAG_SLOTS)
+      Stockpile:InitializeInventoryData()
+      Stockpile:ScanBags()
     end
   end,
 
+  ["PLAYER_LOGOUT"] = function()
+    -- Stockpile:ScanBags()
+  end,
+
   ["BANKFRAME_CLOSED"] = function()
-    StockpileInventoryData[Stockpile.realm][Stockpile.character] = {}
-    Stockpile_ScanBags(-1, NUM_BAG_SLOTS + NUM_BANKBAGSLOTS)
+    Stockpile:ScanBags()
+    Stockpile:ScanBank()
   end,
 
 }
@@ -35,68 +39,126 @@ Stockpile.actions = {
 -- STOCKPILE FUNCTIONS
 -- -------------------------
 
-function Stockpile_InitializeInventoryData()
+function Stockpile:InitializeInventoryData()
   if (StockpileInventoryData == nil) then
-    StockpileInventoryData = {}
+    StockpileInventoryData = {
+      [Stockpile.realm] = {
+        [Stockpile.character] = {
+          bank = {},
+          bags = {}
+        }
+      }
+    }
+  elseif (StockpileInventoryData[Stockpile.realm] == nil) then
+    StockpileInventoryData[Stockpile.realm] = {
+      [Stockpile.character] = {
+        bank = {},
+        bags = {}
+      }
+    }
+  elseif (StockpileInventoryData[Stockpile.realm][Stockpile.character] == nil) then
+    StockpileInventoryData[Stockpile.realm][Stockpile.character] = {
+      bank = {},
+      bags = {}
+    }
   end
 end
 
-function Stockpile_ScanBags(start, stop)
-  for bag = start, stop do
-    Stockpile_ScanBag(bag)
-  end
-  if (start < 1) then
-    Stockpile_ScanBag(REAGENTBANK_CONTAINER)
+function Stockpile:ScanBags()
+  StockpileInventoryData[Stockpile.realm][Stockpile.character]['bags'] = {}
+  for bag = 0, 4 do
+    Stockpile:ScanBag(bag, false)
   end
 end
 
-function Stockpile_ScanBag(bag)
+function Stockpile:ScanBank()
+  StockpileInventoryData[Stockpile.realm][Stockpile.character]['bank'] = {}
+  Stockpile:ScanBag(-1, true)
+  Stockpile:ScanBag(REAGENTBANK_CONTAINER, true)
+  for bag = 5, 11 do
+    Stockpile:ScanBag(bag, true)
+  end
+end
+
+function Stockpile:ScanBag(bag, isBankScan)
   for slot = 1, GetContainerNumSlots(bag) do
     local item = GetContainerItemLink(bag, slot)
     if (item ~= nil) then
-      local itemId = Stockpile_GetItemIdFromLink(item)
-      StockpileInventoryData[Stockpile.realm][Stockpile.character][itemId] = GetItemCount(item, true)
+      local itemId = Stockpile:GetItemIdFromLink(item)
+      local itemCount = GetItemCount(item, isBankScan)
+      local stockpileBucket = Stockpile:GetStockpileBucket(isBankScan)
+
+      if (isBankScan and StockpileInventoryData[Stockpile.realm][Stockpile.character]['bags'][itemId] ~= nil) then
+        itemCount = itemCount - StockpileInventoryData[Stockpile.realm][Stockpile.character]['bags'][itemId]
+      end
+
+      local itemName = string.match(item, "%[(.*)%]")
+      if (itemName == 'Iron Horde Scraps') then
+        print(itemName .. '(' .. stockpileBucket .. '): ' .. itemCount)
+      end
+
+      StockpileInventoryData[Stockpile.realm][Stockpile.character][stockpileBucket][itemId] = itemCount
     end
   end
 end
 
-function Stockpile_GetItemIdFromLink(link)
+function Stockpile:GetStockpileBucket(isBank)
+  if (isBank) then
+    return 'bank'
+  else
+    return 'bags'
+  end
+end
+
+function Stockpile:GetItemIdFromLink(link)
   return string.match(link, "item:(%d+)")
 end
 
-function Stockpile_GetStockpiledItems(itemId)
+function Stockpile:GetStockpiledItems(itemId)
   local items = {}
 
   for character, list in pairs(StockpileInventoryData[Stockpile.realm]) do
-    if (character ~= Stockpile.character and list[itemId] ~= nil) then
-      items[character] = list[itemId]
+    if (character ~= Stockpile.character) then 
+      if (list.bags[itemId] ~= nil or list.bank[itemId] ~= nil) then
+        local count = 0
+
+        if (list.bags[itemId] ~= nil) then
+          count = count + list.bags[itemId]
+        end
+
+        if (list.bank[itemId] ~= nil) then
+          count = count + list.bank[itemId]
+        end
+
+        items[character] = count
+      end
     end
   end
 
   return items
 end
 
+-- -------------------------
+-- TOOLTIP HOOK
+-- -------------------------
+
 local function Stockpile_OnTooltipSetItem(tooltip)
   if not tooltipLinesAdded then
     local itemName, itemLink = tooltip:GetItem()
-    local itemId = Stockpile_GetItemIdFromLink(itemLink)
-    local items = Stockpile_GetStockpiledItems(itemId)
+    local itemId = Stockpile:GetItemIdFromLink(itemLink)
+    local items = Stockpile:GetStockpiledItems(itemId)
 
     for character, count in pairs(items) do
       tooltip:AddLine(character .. ": " .. count)
     end
 
-    tooltipLinesAdded = true
+    Stockpile.tooltipLinesAdded = true
   end
 end
 
 local function Stockpile_OnTooltipCleared()
-  tooltipLinesAdded = false
+  Stockpile.tooltipLinesAdded = false
 end
-
--- -------------------------
--- TOOLTIP HOOK
--- -------------------------
 
 GameTooltip:HookScript("OnTooltipSetItem", Stockpile_OnTooltipSetItem)
 GameTooltip:HookScript("OnTooltipCleared", Stockpile_OnTooltipCleared)
@@ -106,13 +168,11 @@ GameTooltip:HookScript("OnTooltipCleared", Stockpile_OnTooltipCleared)
 -- -------------------------
 
 function Stockpile_OnLoad()
-
   for event, action in pairs(Stockpile.actions) do
     Stockpile:RegisterEvent(event)
   end
 
   Stockpile:SetScript("OnEvent", Stockpile.OnEvent)
-
 end
 
 -- -------------------------
@@ -120,9 +180,7 @@ end
 -- -------------------------
   
 function Stockpile:OnEvent(event, arg1, arg2)
-
   Stockpile.actions[event](arg1, arg2)
-
 end
 
 
